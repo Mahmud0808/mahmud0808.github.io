80936e35e9338483d73f9eaed758b9110e367fc8
DrDisagree <crazymahmud08@gmail.com>
2024-11-04 19:12:21 +0600

    Update about section

--- src/components/ui/AuthorImage.tsx
+++ src/components/ui/AuthorImage.tsx
@@ -6,15 +6,17 @@ type Props = { src: string; alt: string };
 
 const AuthorImage = ({ src, alt }: Props) => {
   return (
-    <div className="relative w-64 h-64 group sm:w-auto sm:h-auto">
+    <div className="relative w-64 h-64 group sm:w-auto sm:h-auto !z-40">
       <Image
         src={`${prefix}${src}`}
         alt={alt}
         width={300}
         height={300}
-        className="rounded shadow-xl"
+        objectFit="cover"
+        objectPosition="center"
+        className="rounded-xl shadow-xl drop-shadow-corner"
       />
-      <div className="absolute inset-0 border-[3px] z-[-1] rounded border-accent translate-x-6 translate-y-6 group-hover:translate-x-4 group-hover:translate-y-4 duration-150 hidden sm:block"></div>
+      <div className="absolute inset-0 border-[3px] z-[-1] rounded-xl border-accent translate-x-6 translate-y-6 group-hover:translate-x-4 group-hover:translate-y-4 duration-150 hidden sm:block"></div>
     </div>
   );
 };
--- src/lib/content/about.ts
+++ src/lib/content/about.ts
@@ -4,7 +4,7 @@ export const aboutSection: AboutSectionType = {
   title: 'about me',
   paragraphs: [
     'I’m a passionate developer eager to explore every tech stack, especially in Android and web development.',
-    'My main focus these days is learning new technologies and finding a decent job.',
+    'My main focus these days is learning new technologies and finding a decent job. I thrive on solving challenging problems and continuously improving my skills. My goal is to contribute to innovative projects and grow within a dynamic team.',
   ],
   list: {
     title: 'Here are a few languages I’ve been working with recently:',
@@ -21,5 +21,5 @@ export const aboutSection: AboutSectionType = {
       'Next.js',
     ],
   },
-  img: '/images/mahmudul-hasan.png',
+  img: '/images/mahmudul-hasan.jpg',
 };
